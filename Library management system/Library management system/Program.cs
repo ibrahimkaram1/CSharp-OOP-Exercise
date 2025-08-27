a17cffae5540f3b127766fed9c1d2db6fd9dbf2f
ibrahimkaram1 <hime1792004@gmail.com>
2025-08-27 06:18:07 +0300

    Update Program.cs

--- Library management system/Library management system/Program.cs
+++ Library management system/Library management system/Program.cs
@@ -8,17 +8,13 @@ namespace Library_management_system
         {
            Librarian librarian = new Librarian();
             Library library = new Library();
-            // Adding books to the library
             librarian.AddBook();
             librarian.AddBook();
             librarian.AddBook();
             librarian.DisplayAvailableBooks(library);
             librarian.BorrowBook(library, "9780451524935");
-            // Display available books after borrowing
             librarian.DisplayAvailableBooks(library);
-            // Returning a book
             librarian.ReturnBook(library, "9780451524935");
-            // Display available books after returning
             librarian.DisplayAvailableBooks(library);
         }
     }
